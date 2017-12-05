4cbcc5a49cdc35b7ae398d4b36dcf35faede5293
Fb961010 <919451048@qq.com>
2017-12-05 22:46:26 +0800

    微信 测试

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -3,8 +3,46 @@ namespace frontend\controllers;
 use yii;
 use yii\web\Controller;
 class IndexController extends Controller{
-
+    public  $enableCsrfValidation=false;
+    public $xml;
     public function actionIndex(){
-       echo  $_GET['echostr'];
+       if($echostr = yii::$app->request->get('echostr')){
+           echo $echostr;
+           exit();
+       }
+       $this->xml = simplexml_load_string($GLOBALS['HTTP_RAW_POST_DATA']);
+       switch($this->xml->MsgType){
+           case 'text':
+                $this->responeText();
+                break;
+            case 'event':
+                $this->responeEvent();
+                break;
+       }
+    }
+
+    /**
+     * 文本处理方法
+     * 
+     */
+    public function responeText(){
+        $con ="";
+        switch($this->xml->Content){
+            case'你好':
+                $con = "号";
+                break;
+            case "年后吗":
+                $con = "没毛病";
+                break;
+            default:
+                $con = "lalal[鄙视]";
+                break;
+        }
+        $this->sendResponse($con);
+    }
+
+    public function sendResponse($con,$msg_type='text'){
+       $xml='<xml><ToUserName><![CDATA['.$this->xml->FormUser.']]></ToUserName><FromUserName><![CDATA['.$this->xml->ToUser.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA['.$msg_type.']]></MsgType><Content><![CDATA['.$con.']]></Content></xml>';
+        echo $xml;
     }
 }
