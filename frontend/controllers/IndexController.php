5f3789321ceb594cfbed7c808c9c8b7c2274c7c6
Fb961010 <919451048@qq.com>
2017-12-05 16:25:03 +0800

    提交

---
+++ frontend/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+namespace frontend\controllers;
+use yii;
+use yii\web\Controller;
+class IndexController extends Controller{
+
+    public function actionIndex(){
+        echo "HEllO  WORLD",'HELLO GIT';
+    }
+}
