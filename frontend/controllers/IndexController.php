340f3832e39e6b5f3c0cc2ace9f7a2c64f2da259
Fb961010 <919451048@qq.com>
2017-12-05 20:38:50 +0800

    微信接口对接测试第一步

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -5,6 +5,6 @@ use yii\web\Controller;
 class IndexController extends Controller{
 
     public function actionIndex(){
-        echo "HEllO  WORLD",'HELLO GIT';
+       echo  yii::$app->request->get('echoStr') && $_GET['echoStr'];
     }
 }
