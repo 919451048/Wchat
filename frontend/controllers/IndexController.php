3b3a5b37a4621b13c339ce43e23cc21b35c26897
Fb961010 <919451048@qq.com>
2017-12-05 21:32:06 +0800

    测试  echo Str对接

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -5,6 +5,6 @@ use yii\web\Controller;
 class IndexController extends Controller{
 
     public function actionIndex(){
-       echo  yii::$app->request->get('echoStr') && $_GET['echoStr'];
+       echo  $_GET['echoStr'];
     }
 }
